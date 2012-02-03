0f02713561a0fa01f016c60e0fc6c72d14871626
Stefan Rusu <saltwaterc@gmail.com>
2012-02-03 12:11:19 +0200

    Modified a test so it wouldn't crash on node 0.6.x due to changes in errrno values.

--- tests/invalid-file-stream.js
+++ tests/invalid-file-stream.js
@@ -17,7 +17,6 @@ fs.chmodSync(path, 0100);
 ftp.get('ftp://127.0.0.1/foo.txt', path, function (err, res) {
 	callback = true;
 	assert.ok(err instanceof Error);
-	assert.equal(err.errno, 13);
 	assert.deepEqual(err.code, 1);
 });
 
